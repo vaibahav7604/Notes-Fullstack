7882d6ed2561aad7d5021c1bc5b4264ee65721c5
Meghani Vaibhav <vaibhavmeghani79@gmail.com>
2025-04-24 15:36:09 +0530

    2 day of dploying

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,18 +81,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'postgresql://db_prig_user:wSxYf4DLwMrvVpYkSfmkrhEs32agjBEt@dpg-d04faouuk2gs73bg0k5g-a/db_prig'),
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default':{
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR/ 'db.sqlite3',
+#     }
+    
+# }
+
+# DATABASES["default"] = dj_database_url.parse("postgresql://db_prig_user:wSxYf4DLwMrvVpYkSfmkrhEs32agjBEt@dpg-d04faouuk2gs73bg0k5g-a.oregon-postgres.render.com/db_prig")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
