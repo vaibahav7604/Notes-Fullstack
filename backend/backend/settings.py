034ad62ce76709f755ac5b0b47e04e044def6d64
Meghani Vaibhav <vaibhavmeghani79@gmail.com>
2025-04-24 15:56:49 +0530

    2 day of dploying

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,12 +13,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-470p*ls=@&e%%g)fg3uvjq9^zsbho7y7ji8^337c70#a&c(n%q')
+SECRET_KEY = os.getenv('SECRET_KEY', 'wSxYf4DLwMrvVpYkSfmkrhEs32agjBEt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-470p*ls=@&e%%g)fg3uvjq9^zsbho7y7ji8^337c70#a&c(n%q')
 
 # ✅ Correct
 REST_FRAMEWORK = {
